dbd10a7960fefae7c89d7a5cd74f8c41b292c5ba
Natalia <Deva2014@yandex.ru>
2022-05-17 20:03:46 +0300

    Задача 10

---
+++ Ex001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int num2 = ((num % 100) / 10); // 1 способ
+Console.WriteLine(num + " -> " + num2);
+
+string text = num.ToString(); // 2 способ
+Console.WriteLine(text + " -> " + text[1]);
+
