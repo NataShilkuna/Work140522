3f92c24e775b31a88c7f5e2c0921447bee172d4d
Natalia <Deva2014@yandex.ru>
2022-05-17 20:23:04 +0300

    Задача 15

---
+++ Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+int day = new Random().Next(1, 7);
+Console.WriteLine(day);
+
+//1 способ
+//if(day<=5){Console.WriteLine(day + " -> рабочий");}  
+//else{Console.WriteLine(day + " -> выходной");}
+
+//2 способ 
+Console.WriteLine(day<=5 ? day + " -> рабочий" : day + " -> выходной");
